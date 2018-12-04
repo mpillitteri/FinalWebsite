d668bf3d56866ecc3dccdbe80b03244bd8ae9358
Maria Pillitteri <43287010+mpillitteri@users.noreply.github.com>
2018-12-03 23:49:27 -0500

    footer tweaks
    footer tweaks

--- saferbeauty_script.js
+++ saferbeauty_script.js
@@ -1,3 +1,16 @@
-var x = document.getElementById("contact");
-var createform = document.createElement('form');
-x.appendChild(createform);
+
+function contactUs() {
+  document.getElementById("contactinfo").innerHTML = "Email: safterbeauty@gmail.com";
+}
+
+//function addWords(){
+//    var words = document.getElementById("stuff").value;
+//}
+
+function subscribeForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Enter a valid name");
+        return false;
+    }
+}
